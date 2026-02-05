18866c02513f49cb2080b88535bd4eb8cdb21690
plevagen <varkginglouh@gmail.com>
2026-02-05 20:01:39 +0500

    fd

---
+++ src/main/java/com/example/musiclibrary/Artist.java
@@ -0,0 +1,36 @@
+package com.example.musiclibrary;
+
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+public class Artist {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String name;
+
+    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Song> songs = new ArrayList<>();
+
+    public Artist(){}
+    public Artist(String name){
+        this.name = name;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public List<Song> getSongs(){
+        return songs;
+    }
+    public void addSong(Song s){
+        songs.add(s);
+        s.setArtist(this);
+    }
+}
